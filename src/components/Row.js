1068c72055d8ad210a305363f88c815ec9d5073c
umutplbyk96 <padostudio@hotmail.com>
2021-04-22 11:08:06 +0300

    row.js edited for more clean code

--- src/components/Row.js
+++ src/components/Row.js
@@ -22,9 +22,7 @@ const Row = ({ title, fetchUrl, isLarge }) => {
         {movies.map((movie) => {
           return (
             <img
-              src={`${baseImgUrl}${
-                isLarge ? movie.poster_path : movie.backdrop_path
-              }`}
+              src={baseImgUrl+( isLarge ? movie.poster_path : movie.backdrop_path)}
               className={`row-poster ${isLarge && "row_posterLarge"}`}
               key={movie.id}
               alt={movie.original_name}
